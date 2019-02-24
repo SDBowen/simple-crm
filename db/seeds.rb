ffe33b0f2a4dc093cdefad418036492d63c8f8ce
sdbowen <sdbowen.git@gmail.com>
2019-02-24 16:04:41 -0700

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'csv'
+
+def parse_csv(file_path)
+  CSV.foreach(file_path, headers: true) do |row|
+    organization = lookup_or_create(row)
+
+    Person.create!(name: row['name'], job_title: row['job_title'], email_address: row['email_address'],
+                   phone: row['phone'], organization: organization)
+  end
+
+  completed_message
+end
+
+def lookup_or_create(row)
+  org_detail = { name: row['organization'], phone: row['organization_phone'], domain: row['domain'] }
+  address_detail = { street: row['street'], city: row['city'], state: row['state'], zip: row['zip'] }
+  organization = Organization.where(org_detail).first
+
+  unless organization
+    organization = Organization.create!(org_detail)
+    organization.create_address(address_detail)
+  end
+
+  organization
+end
+
+def completed_message
+  puts "#{Organization.count} organizations added"
+  puts "#{Address.count} addresses added"
+  puts "#{Person.count} people added"
+end
+
+parse_csv("#{Rails.root}/db/crm_exercise_data.csv")
