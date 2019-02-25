995751b93a20c9d56a1c64283af659e6638b3ca7
sdbowen <sdbowen.git@gmail.com>
2019-02-24 21:46:00 -0700

    Prevent empty string from saving in db

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,9 +4,16 @@ class Person < ApplicationRecord
   belongs_to :organization
   has_one :address, through: :organization
 
+  before_save :empty_phone_to_nil
   validates :name, :job_title, :email_address, :organization, presence: true
 
   def self.people_count(org_id)
     Person.where(organization: org_id).count
+  end
+
+  private
+
+  def empty_phone_to_nil
+    self.phone = nil if phone.presence.nil?
   end
 end
